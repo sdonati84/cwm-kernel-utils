1b388d134c89fe46efa31b287019c4788ac6054f
Stefano Donati <sdonati84@gmail.com>
2012-03-30 15:17:29 +0200

    Prettify script

--- build_boot.sh
+++ build_boot.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 export ANDROID_BUILD_TOP=/home/steve/android/system
-export DEVICE_DIR=/home/steve/android/system/device/samsung/galaxysmtd
+KERNEL_DIR=/home/steve/android/system/kernel/samsung/aries
+PRODUCT=galaxysmtd
+DEVICE_DIR=/home/steve/android/system/device/samsung/$PRODUCT
+CWM_SCRIPTS_DIR="/home/steve/android/cwm-kernel-utils"
 
-cp $ANDROID_BUILD_TOP/out/target/product/galaxysmtd/ramdisk.img .
-cp $ANDROID_BUILD_TOP/out/target/product/galaxysmtd/ramdisk-recovery.img .
+cd $KERNEL_DIR
+./build.sh $PRODUCT
+
+cd $CWM_SCRIPTS_DIR
+cp $ANDROID_BUILD_TOP/out/target/product/$PRODUCT/ramdisk.img .
+cp $ANDROID_BUILD_TOP/out/target/product/$PRODUCT/ramdisk-recovery.img .
 cp $DEVICE_DIR/kernel zImage
 cp $DEVICE_DIR/*.ko cwm/system/lib/modules
 ./mkshbootimg.py boot.img zImage ramdisk.img ramdisk-recovery.img
